2a198f9136cfd81e63241065f9a35de1ee266d82
Coltz889 <connor.kian@education.nsw.gov.au>
2025-03-11 00:55:49 +0000

    game

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,30 @@
 player_name = prompt("enter name");
 alert("Hello " + player_name);
-player_guess = prompt("rock(1) paper(2) scissors(3)");
-computer_guess = randomIntFromInterval(1, 3);
-const game = ["rock", "paper", "scissors"];
-alert("u guessed " + game[player_guess - 1]);
-alert("the computer guessed " + game[computer_guess - 1]);
-if (player_guess == computer_guess) {
-  document.getElementById("user_feedback").innerHTML = "<em>draw</em>";
-} else if (player_guess == 3 && computer_guess == 1) {
-  document.getElementById("user_feedback").innerHTML =
-    "<em>your buns at game</em>";
-} else if (player_guess == 1 && computer_guess == 3) {
-  document.getElementById("user_feedback").innerHTML = "<em>your winner</em>";
-} else if (player_guess > computer_guess) {
-  document.getElementById("user_feedback").innerHTML = "<em>your winner</em>";
-} else {
-  document.getElementById("user_feedback").innerHTML =
-    "<em>your buns at the game</em>";
+mybool = true;
+gamerScore = "1";
+computerScore = 1;
+sumscore = gamerScore + computerScore;
+alert(sumscore);
+function myGame() {
+  //i coded by myself
+  player_guess = prompt("rock(1) paper(2) scissors(3)");
+  computer_guess = randomIntFromInterval(1, 3);
+  const game = ["rock", "paper", "scissors"];
+  alert("u guessed " + game[player_guess - 1]);
+  alert("the computer guessed " + game[computer_guess - 1]);
+  if (player_guess == computer_guess) {
+    document.getElementById("user_feedback").innerHTML = "<em>draw</em>";
+  } else if (player_guess == 3 && computer_guess == 1) {
+    document.getElementById("user_feedback").innerHTML =
+      "<em>your buns at game</em>";
+  } else if (player_guess == 1 && computer_guess == 3) {
+    document.getElementById("user_feedback").innerHTML = "<em>your winner</em>";
+  } else if (player_guess > computer_guess) {
+    document.getElementById("user_feedback").innerHTML = "<em>your winner</em>";
+  } else {
+    document.getElementById("user_feedback").innerHTML =
+      "<em>your buns at the game</em>";
+  }
 }
 //if (player_guess)
 function randomIntFromInterval(min, max) {
