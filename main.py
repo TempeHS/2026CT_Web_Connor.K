79dbcae7b5559e8e58d004127fdd694eaea4ca47
Coltz889 <connor.kian@education.nsw.gov.au>
2025-05-21 01:54:53 +0000

    cards

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/index.html')
-@app.route('/reasons.html')
 @app.route('/')
 def index():
-    return render_template("index.html"), 200
-def reasons():
-    return render_template("/workspaces/2026CT_Web_Connor.K/templates/reasons.html"), 200
+    card_data = (
+        ("Getting Started", "How to begin your website","link","static/images/logo.png"),
+        ("Examples of Good Web Design", "Here are some examples","link","static/images/logo.png"),
+        ("Ways You Can Learn", "The Learning Ways","link","static/images/logo.png"),
+    )
+    return render_template("index.html", cards = card_data), 200
 if __name__ == '__main__':
     app.run(debug=True)
