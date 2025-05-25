33d3334ed8027aa663f1073f4f6160cd36f4272f
Coltz889 <connor.kian@education.nsw.gov.au>
2025-05-25 23:36:13 +0000

    Cards

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     card_data = (
-        ("Getting Started", "How to begin your website","link","static/images/logo.png"),
-        ("Examples of Good Web Design", "Here are some examples","link","static/images/logo.png"),
-        ("Ways You Can Learn", "The Learning Ways","link","static/images/logo.png"),
+        ("Getting Started", "How to begin your website","link","static/images/card1.png"),
+        ("Examples of Good Web Design", "Here are some examples","link","static/images/card2.png"),
+        ("Ways You Can Learn", "The Learning Ways","link","static/images/card3.png"),
+        ("Ways You Can Learn", "The Learning Ways","link","static/images/card4.png"),
+        ("Flash!", "Oof owie my eyes","Burn my eyes","static/images/card5.png"),
+        ("Ways You Can Learn", "The Learning Ways","link","static/images/card6.png"),
     )
     return render_template("index.html", cards = card_data), 200
 if __name__ == '__main__':
