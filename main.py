b2ceea02448fc96d0d3d80b057d09862f12352f9
Coltz889 <connor.kian@education.nsw.gov.au>
2025-06-02 02:49:43 +0000

    Beginning

--- main.py
+++ main.py
@@ -7,13 +7,28 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     card_data = (
-        ("Getting Started", "How to begin your website","link","static/images/card1.png"),
+        ("Getting Started", "How to begin your website","/starting.html","static/images/card1.png"),
         ("Examples of Good Web Design", "Here are some examples","link","static/images/card2.png"),
         ("Ways You Can Learn", "The Learning Ways","link","static/images/card3.png"),
         ("Ways You Can Learn", "The Learning Ways","link","static/images/card4.png"),
-        ("Flash!", "Oof owie my eyes","Burn my eyes","static/images/card5.png"),
+        ("Flash!", "Oof owie my eyes","link","static/images/card5.png"),
         ("Ways You Can Learn", "The Learning Ways","link","static/images/card6.png"),
     )
     return render_template("index.html", cards = card_data), 200
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html'),200
+
+
+@app.route('/starting.html')
+def starting():
+    return render_template('starting.html'),200
+
+@app.route('/credits.html')
+def credits():
+    return render_template('credits.html'),200
+
 if __name__ == '__main__':
     app.run(debug=True)
