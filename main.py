a18242e8448b38101750516ec18dfc8e476a9cf7
Coltz889 <connor.kian@education.nsw.gov.au>
2025-06-03 01:55:02 +0000

    Progress bar

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     card_data = (
         ("Getting Started", "How to begin your website","/starting.html","static/images/card1.png"),
-        ("Examples of Good Web Design", "Here are some examples","link","static/images/card2.png"),
+        ("Examples of Good Web Design", "Here are some examples","/goodweb.html","static/images/card2.png"),
         ("Ways You Can Learn", "The Learning Ways","link","static/images/card3.png"),
         ("Ways You Can Learn", "The Learning Ways","link","static/images/card4.png"),
         ("Flash!", "Oof owie my eyes","link","static/images/card5.png"),
@@ -29,6 +29,10 @@ def starting():
 @app.route('/credits.html')
 def credits():
     return render_template('credits.html'),200
+
+@app.route('/goodweb.html')
+def goodweb():
+    return render_template('goodweb.html'),200
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/progressbar.js
@@ -0,0 +1,13 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  checkscrolly();
+};
+
+function checkscrolly() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
