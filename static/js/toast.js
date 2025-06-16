c30d5ebc3c4748d1f92ba78655793df650dc41c5
Coltz889 <connor.kian@education.nsw.gov.au>
2025-06-16 02:49:30 +0000

    stuff

--- static/js/toast.js
+++ static/js/toast.js
@@ -1,7 +1,6 @@
 const toastLiveExample = document.getElementById("liveToast");
-
 function popup() {
   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
   toastBootstrap.show();
 }
-setInterval(popup, 5000);
+popup();
